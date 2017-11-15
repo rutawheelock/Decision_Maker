e42e66017702ce8ec6341b8b2210aafd1c7f159e
Ruta Wheelock <wheel477@umn.edu>
2017-11-14 21:31:02 -0600

    Added program descriptions and detailed comments

--- decision-list-test.py
+++ decision-list-test.py
@@ -6,7 +6,35 @@ Program 2: Assigns senses to test data
 Author: Ruta Wheelock
 Date: 11/15/2017
 
-Description
+Description:
+This program examins the content of test data and assigns a word sense
+to the target word based on decision list.
+
+To run the program, execute:
+$ ./decision-list-test.py decision-list.txt test-data.txt
+The second command line argument is a name of the decision list file.
+Third argument is the training data file.
+
+Program writes answers to standard output.
+To write answers in a file, execute:
+$ ./decision-list-test.py decision-list.txt test-data.txt > answers.txt
+
+Algorithm:
+Adjacent words to the target word are extracted from the test file.
+Then each word is looked at the decision list starting from the top.
+When one of the words if found in the decision list, approprite sense is 
+assigned to the targed word.
+
+Example:
+Sample output (first 5 lines) to the following command:
+$ ./decision-list-test.py interest-n-decision-list.txt interest-n-test.txt
+<answer instance="interest-n.int1585" senseid="interest_5"/>
+<answer instance="interest-n.int1724" senseid="interest_5"/>
+<answer instance="interest-n.int1555" senseid="interest_5"/>
+<answer instance="interest-n.int2260" senseid="interest_5"/>
+<answer instance="interest-n.int1847" senseid="interest_5"/>
+
+Output is formatted according to the key file provided for this assignment.
 
 """
 
@@ -19,22 +47,29 @@ def main():
         print "Please provide decision list and test data as a second and third command line arguments."
         exit(1);
 
+    # Read in file names
     decision_list_file = str(sys.argv[1])
     test_file = str(sys.argv[2])
 
+    # Open files
     decision_list = open(decision_list_file, 'r')
-    decisions = decision_list.read().splitlines()
     test_data = open(test_file, 'r')
+
+    # Split decision list into lines
+    decisions = decision_list.read().splitlines()
 
     # Declare variables for input processing
     id_code = ''
     previous_line = 'previouslinehere'
     senseid = ''
 
+    # Look through the test data file
     for line in test_data:
+        # Retrieve an instance code from <instance> tag
         if line[:9] == '<instance':
             id_code = line.partition('instance id=')[2].rstrip(">\n")
 
+        # Find adjacent words to the target word
         if previous_line[:9] == '<context>':
             left_side = line.partition('<head>')[0].split()
             right_side = line.partition('</head>')[2].split()
@@ -43,16 +78,23 @@ def main():
 
             found_match = False
 
+            # Loop through the decision list until
+            # one of the adjacent words is found
             for decision in decisions:
+                # Split the line
                 dec_list = decision.split()
                 keyword = dec_list[1]
+                # Loop through adjacent word list
                 for near_word in nearby_words:
+                    # If the word is found in decision list,
+                    # save the corresponding sense
                     if near_word == keyword:
                         senseid = '"{}"'.format(dec_list[2])
                         found_match = True
                 if (found_match):
                     break
 
+            # Print the answer
             print "<answer instance={} senseid={}/>".format(id_code, senseid)
             
         previous_line = line
--- decision-list-train.py
+++ decision-list-train.py
@@ -7,6 +7,42 @@ Author: Ruta Wheelock
 Date: 11/15/2017
 
 Description:
+This program builds decision list using training data for word sense disambiguation.
+Program expects training data in a specfic format that was provided for this assignment.
+
+To run the program, execute:
+$ ./decision-list-train.py trainig-data.txt
+The second command line argument is a name of training data, which is located in the same
+directory where this program.
+
+Program writes decision list to the standard output.
+To write the decision list in a file, execute:
+$ ./decision-list-train.py trainig-data.txt > decision-list.txt
+
+Algorithm:
+Decision list is based on adjacent word likelihood to predict a word sense to the target word.
+The program collects adjacent words to the target word and calculates
+their log-likelihood as decribed in David Yarowsky paper "Decision Lists for Lexical Ambiguity
+Resolution". If multiple words have the same log-likelihood, adjacent word count is used for ranking.
+
+Example:
+Sample output (first 5 lines) to the following command:
+$ ./decision-list-train.py interest-n-train.txt 
+2.30258509299 short interest_5 36
+2.30258509299 minority interest_5 22
+2.30258509299 has interest_5 17
+2.30258509299 equity interest_5 13
+2.30258509299 open interest_5 8
+
+Each line in decision list has four items:
+The first item is a log-likelihood of the adjacent word to determine word sense.
+Second item an adjacent word.
+Third item is a word sense that the adjacent word predicts.
+Fourth item is a number of instances the adjacent word was encountered.
+
+Limitations:
+The program is hardcoded to consider only two senses to disambiguate.
+If the training data contains more senses, only two will be considered.
 
 """
 
@@ -24,60 +60,77 @@ def main():
 
     # Save training data file name
     train_file = str(sys.argv[1])
-    # Get the word for sense disambiguation
-    main_word = train_file.split('-')[0]
+
+
+    #----------------------------
+    # Process training data file
+    #----------------------------
 
     # Open the training file
     train_data = open(train_file, 'r')
-
-    # Declare scope variables for line operations
+    
+    # Declare variables for line operations
     senses_list = []
-    previous_line = 'somethinghere'
-    test_count = 0
+    previous_line = 'previouslinehere'
     sense = ''
     collocation_tuples = []
-    
+
+    # Read in every line in training data
     for line in train_data:
+        # Look for word sense in <answer> tag and save it
         if line[0:7] == '<answer':
             sense = line.split('senseid=')[1][1:-4]
             senses_list.append(sense)
 
+        # If the previous line was <context>, this line should contain the target word
         if previous_line[:9] == '<context>':
             left_side = line.partition('<head>')[0].split()
             right_side = line.partition('</head>')[2].split()
 
+            # Strip special characters from adjacent words and save words in a list
             nearby_words = [left_side[-1].strip(',.!"-?'), right_side[0].strip(',.!"-?')]
 
+            # If the adjacent word is not an empty string, append it to collocation_tuples
+            # with corresponding target word sense
             for word in nearby_words:
                 if word != '':
                     collocation_tuples.append((word,sense))
-                    #print collocation_tuples[-1]
             
         previous_line = line
+
+
+    # Close training data file
+    train_data.close()
 
 
     #----------------------------
     # Build decision list
     #----------------------------
-    # !!!! Note - Hard coded for only 2 senses!!!
 
     decision_list = []
 
+    # Create a set of adjacent word so that each word can be looked
+    # at only once
     set_of_nearby_words = set(x[0] for x in collocation_tuples)
+    
+    # Create a set of senses and save them in variables
     set_of_senses = set(x[1] for x in collocation_tuples)
     sense_1 = set_of_senses.pop()
     sense_2 = set_of_senses.pop()    
-        
+
+    # Loop through a set of adjacent words
     for word in set_of_nearby_words:
+        # Count how many times the word was encountered with one of the senses
+        # in training data
         sense_1_count = collocation_tuples.count((word, sense_1))
         sense_2_count = collocation_tuples.count((word, sense_2))
         total_count = sense_1_count + sense_2_count
 
+        # Calculate probability for each sense
         p_sense_1 = sense_1_count/total_count
         p_sense_2 = sense_2_count/total_count
-#        print "P1: {}, P2: {}".format(p_sense_1, p_sense_2)
 
-        # smoothing
+        # Apply smoothing for zero probalilities
         # According to Yarowski, p: 0.1 - 0.25 is appropriate
         if p_sense_1 == 0:
             p_sense_1 = 0.1
@@ -85,43 +138,29 @@ def main():
         if p_sense_2 == 0:
             p_sense_2 = 0.1
 
+        # Determine which sense was encountered more oftem
         classification = sense_1
         if sense_2_count > sense_1_count:
             classification = sense_2
-            
+
+        # Calculate log-likelihood for this word
         log_likelihood = abs(log(p_sense_1/p_sense_2))
 
+        # Determine whether the word is informative
+        # If the log-likelihood is 0, adjacent word is not informative
+        # If the word is informative, add it to the decision list
         if log_likelihood != 0.0 and total_count > 1:
             decision_list.append((log_likelihood, word, classification, total_count))
         
- #       print "Word: {}, Sense_1: {}, Sense_2: {}, Total: {}".format(word, sense_1_count, sense_2_count, total_count)
- #       print "P1: {}, P2: {}, log_lkh: {}, class: {}".format(p_sense_1, p_sense_2, log_likelihood, classification)
 
-
-        
-    # Find how many senses were found
-#    senses_set = set(senses_list)
-#    print "Number of instances: {}".format(len(senses_list))
-#    for sense in senses_set:
-#        print "Number of {meaning} senses: {count}".format(meaning=sense,
-#                                                           count=senses_list.count(sense))
-
+    # Sort the decision list based on log-likelihood and the number of times
+    # an adjacent word was encountered
     sorted_decision_list = sorted(decision_list, key=itemgetter(0,3), reverse=True)
-    
+
+    # Write the decision list to the standard output
     for decision in sorted_decision_list:
         print decision[0], decision[1], decision[2], decision[3]
-        
-
-    # for testing
-    #for entry in collocation_tuples:
-    #    word = entry[0]
-    #    if word == 'a':
-    #        print entry
-
-    
-    # Close training data file
-    train_data.close()
-    
+            
 
 if __name__ == "__main__":
     main()
